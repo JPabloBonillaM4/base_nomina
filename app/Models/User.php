13fb1f57d8f811f57ef031691fc9ef7870a66865
Juan Pablo Bonilla Mendez <jpm_caprico@hotmail.com>
2025-05-23 00:47:19 -0600

    Refactor User model: remove FilamentUser interface and canAccessPanel method

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,12 @@
 
 namespace App\Models;
 
-use Filament\Panel;
-use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Notifications\Notifiable;
-use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -59,12 +56,6 @@ class User extends Authenticatable implements FilamentUser
         'usuario'     => 'Usuario',
        //  Add any other roles you have in your application
     ];
-
-    public function canAccessPanel(Panel $panel): bool
-    {
-        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
-    }
-
     public function producciones()
     {
         return $this->hasMany(ProduccionDiaria::class, 'usuario_id');
